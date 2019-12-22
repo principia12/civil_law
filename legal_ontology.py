22f6f0d23b43adafd6ad15554a20b8a6933ba800
Schin <principia_12@kaist.ac.kr>
2019-12-22 17:48:08 +0900

    initial commit

---
+++ legal_ontology.py
@@ -0,0 +1,13 @@
+class LegalEntity:
+    pass
+    
+class LegalRight:
+    pass
+    
+class LegalRelation:
+    pass
+    
+class LegalStatement:
+    """법의 근거가 되는 조항을 적시하기 위한 wrapper class
+    """
+    pass
